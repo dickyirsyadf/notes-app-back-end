b229abe63eaee96bc2f960b4d8167c34c3121064
dickyirsyadf <dickyirsyadf@gmail.com>
2023-09-28 15:08:29 +0700

    init commit

---
+++ src/routes.js
@@ -0,0 +1,30 @@
+const { addNoteHandler, getAllNotesHandler, getNoteByIdHandler, editNodeByIdHandler, deleteNodeByIdHandler } = require("./handler");
+
+const routes = [
+  {
+    method: 'POST',
+    path: '/notes',
+    handler: addNoteHandler,
+  },
+  {
+    method: 'GET',
+    path: '/notes',
+    handler: getAllNotesHandler,
+  },
+  {
+    method: 'GET',
+    path: '/notes/{id}',
+    handler: getNoteByIdHandler,
+  },
+  {
+    method: 'PUT',
+    path: '/notes/{id}',
+    handler: editNodeByIdHandler,
+  },
+  {
+    method: 'DELETE',
+    path: '/notes/{id}',
+    handler: deleteNodeByIdHandler,
+  }
+];
+module.exports = routes;
